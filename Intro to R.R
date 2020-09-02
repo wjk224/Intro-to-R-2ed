19939bdc8128db54ddc1f394516de40aa08a6160
William Kelly <wjk224@lehigh.edu>
2020-09-02 17:08:31 -0400

    Edited Header

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,10 @@
-# R script to accopamy Intro to R
-# Copy for willkelly 2020
+# R script to accompany Intro to R
+#
+# Written By Troy Adair
+#
+# Copy for Will Kelly 2020
+#
+# Edited by Will Kelly 2020
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
